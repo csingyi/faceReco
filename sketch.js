f545dd14ab5f403b7b3f5685e93781a204cd0b37
csingyi <csingyi@yahoo.com.hk>
2018-09-14 07:16:55 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,154 +1,181 @@
+var canvas1;
+var canvas2;
 var video;
-var button;
-var button2;
-var mycanvas;
 var myImg;
-var myImg2;
+var capImageBtn;
 var inputName;
 var persons = [];
 var forwardParams;
 var allClass;
 var iKnowYou;
 var clearbutton;
+var flag;
+var sketch = function(p) {
+  p.setup = function() {
+    p.createCanvas(480, 320);
 
-var flag = false;
-async function setup() {
-  mycanvas = createCanvas(320, 240);
-  background(51);
-  video = createCapture(VIDEO);
-  video.size(320, 240);
-  video.hide()
-  createP("")
-  button = createButton('setup');
-  button.mousePressed(init);
-  button2 = createButton('capImage');
-  inputName = createInput('');
-  clearbutton=createButton("clear")
-  clearbutton.mousePressed(clearPic);
-  button2.mousePressed(capImage);
-  iKnowYou=createP("")
+    p.background(51);
+  };
 
-//   myImg=createImg("/public/img/raj1.png")
-//   myImg2 = createImg('/public/img/raj5.png');
+  p.draw = function() {
+    var w = 200;
+    var h = 100;
+    var x = 0;
+    var y = 0;
+    for (let index = 0; index < persons.length; index++) {
+      const person = persons[index];
+      let personName = Object.keys(person)[0];
+      let personImg = person[personName];
+      p.image(personImg, x, y, w, h);
+      p.fill(255);
+      p.textSize(40);
+      p.text(personName, x + w / 2, y + h / 3);
+      x = x + w;
+      if (x > 300) {
+        x = 0;
+        y = y + h;
+      }
+    }
+  };
+};
 
+var sketch2 = function(p) {
+  var video;
+  var mycanvas;
+  var imgEle;
+  var rectBox;
+  p.setup = function() {
+    mycanvas = p.createCanvas(480, 320);
+    p.background(51);
+    video = p.createCapture(VIDEO);
+    video.size(480, 320);
+    video.hide();
+  };
+
+  p.faceDectect = async function() {
+    p.image(video.get(), 0, 0, 480, 320);
+    // im=createImg(video.get())
+    // console.log(mycanvas.canvas);
+    imgEle = mycanvas.canvas;
+    // console.log(imgEle)
+    if (flag) {
+      const forwardParams = {
+        scoreThreshold: 0.5,
+        inputSize: 'sm',
+      };
+      const descriptor1 = await faceapi.computeFaceDescriptor(imgEle);
+      //  console.log(descriptor1);
+      const detections = await faceapi.tinyYolov2(imgEle, forwardParams);
+
+      if (detections === undefined || detections.length == 0) {
+        iKnowYou.html("");
+         return
+      }
+
+      const detectionsForSize = detections.map(det =>
+        det.forSize(imgEle.width, imgEle.height),
+      );
+      rectBox = detectionsForSize[0].box;
+
+      p.fill(204, 101, 192, 127);
+      p.stroke(127, 63, 120);
+
+      // A rectangle
+      p.rect(rectBox.x, rectBox.y, rectBox.width, rectBox.height);
+
+      console.log(detectionsForSize);
+
+      var best = getBestMatch(allClass, descriptor1);
+      if (best) {
+        var sayHello = 'hello ' + best.personName;
+
+        console.log(sayHello);
+        iKnowYou.html(sayHello);
+      }
+      // await faceapi.drawDetection(imgEle, detectionsForSize, { withScore: true });
+    }
+  };
+
+  p.draw = function() {
+    // p.image(video, 0, 0, 480, 320);
+    p.faceDectect();
+  };
+};
+
+async function preload() {
   await faceapi.loadTinyYolov2Model('models');
   await faceapi.loadFaceRecognitionModel('models');
-  forwardParams = { scoreThreshold: 0.5, // 'xs' (224 x 224) | 'sm' (320 x 320) | 'md' (416 x 416) | 'lg' (608 x 608) // any number or one of the predifened sizes:
-    inputSize: 'sm' };
+}
+
+function setup() {
+  persons = [];
+  video = createCapture(VIDEO);
+  video.size(480, 320);
+  video.hide();
+  canvas1 = new p5(sketch, 'can1');
+  canvas2 = new p5(sketch2, 'can2');
+  canvas.remove();
+  button = createButton('setup');
+  button.mousePressed(() => {
+    canvas2.faceDectect();
+  });
+  inputName = createInput('');
+  capImageBtn = createButton('capImage');
+  clearbutton = createButton('clear');
+  clearbutton.mousePressed(clearPic);
+  capImageBtn.mousePressed(capImage);
+  iKnowYou = createP('');
 }
 
 function clearPic() {
-
-	persons=[]
-	allClass=[]
+  persons = [];
+  allClass = [];
+  // noCanvas()
 }
 
 function capImage() {
   var myImg = video.get();
   var myname = inputName.value();
-
+  //  console.log(myname)
+  inputName.value('');
+  if (!myname) {
+    return;
+  }
   var myObj = {};
   myObj[myname] = myImg;
 
   persons.push(myObj);
+
+  init();
 }
 
-async function init() {
-	allClass=await initTrainDescriptorsByClass()
-	flag = true;
-	// console.log(allClass);
-	// const descriptor1 = await faceapi.computeFaceDescriptor(myImg2.elt);
+function draw() {}
 
+async function init() {
+  allClass = await initTrainDescriptorsByClass();
+  flag = true;
+  // console.log(allClass);
+  // const descriptor1 = await faceapi.computeFaceDescriptor(myImg2.elt);
 }
 
 async function initTrainDescriptorsByClass() {
   return Promise.all(
-
-	 
     persons.map(async person => {
       personName = Object.keys(person)[0];
-      var descript=await faceapi.computeFaceDescriptor(person[personName].canvas);
+      var descript = await faceapi.computeFaceDescriptor(
+        person[personName].canvas,
+      );
 
       return { descript, personName };
     }),
   );
 }
 
-
 function getBestMatch(descriptorsByClass, queryDescriptor) {
-  
   return descriptorsByClass
     .map(({ descript, personName }) => ({
       distance: faceapi.euclideanDistance(descript, queryDescriptor),
       personName,
     }))
     .reduce((best, curr) => (best.distance < curr.distance ? best : curr));
-}
-
-async function faceDectect() {
-  image(video.get(), 0, 0,320,240);
-  // im=createImg(video.get())
-  // console.log(mycanvas.canvas);
-  imgEle = mycanvas.canvas;
-  const detections = await faceapi.tinyYolov2(imgEle, forwardParams);
-  const detectionsForSize = detections.map(det =>
-    det.forSize(imgEle.width, imgEle.height),
-  );
-
-
-    if (typeof detectionsForSize == 'undefined' || detectionsForSize.length == 0) {
-	  // the array is defined and has at least one element
-	  iKnowYou.html('');
-    }
-//    console.log(detectionsForSize);
-
-   if ((flag && typeof detectionsForSize != 'undefined') && detectionsForSize.length > 0) {
-        const descriptor1 = await faceapi.computeFaceDescriptor(imgEle);
-    //  console.log(descriptor1);
-     
-     var best = getBestMatch(allClass, descriptor1);
-     	 var sayHello = 'hello ' + best.personName;
-     iKnowYou.html(sayHello);
-   } 
-
-     faceapi.drawDetection(imgEle, detectionsForSize, { withScore: true });
-
-//   // console.log(descriptor1)
-//   const descriptor2 = await faceapi.computeFaceDescriptor(person[0]['jack']);
-//   const distance = faceapi.euclideanDistance(descriptor1, descriptor2);
-
-//   if (distance < 0.5) console.log('match');
-//   else console.log('no match');
-
-
-  //   faceapi.drawLandmarks(imgEle, faceLandmarks);
-  // console.log(detectionsForSize[0].relativeBox);
-}
-
-function draw() {
-  //   faceDectect()
-  // faceDectect()
-  // background(255,255,0)
-  // image(video, 0, 0)
-  var w = 80;
-  var h = 60;
-  var x = 0;
-  y = 0;
-
-  for (let index = 0; index < persons.length; index++) {
-    const element = persons[index];
-    var key = Object.keys(element)[0];
-    var img = element[key];
-	image(img, x, y, w, h);
-
-	text(key, x+w/2, y+w/2)
-    x = x + w;
-    if (x > width) {
-      y = y + h;
-      x = 0;
-    }
-  }
-
-  faceDectect();
-
 }
